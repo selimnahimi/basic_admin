c2687baba4bb93e6f91335353f8b7f7c23b75a55
HUNcamper <selimnahimi@gmail.com>
2020-06-15 19:51:29 +0200

    Updated login

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -7,6 +7,10 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use App\Http\Requests;
 
+/* Forms */
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Gregwar\CaptchaBundle\Type\CaptchaType;
 
 class SecurityController extends AbstractController
@@ -20,12 +24,30 @@ class SecurityController extends AbstractController
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
 
+        if ($error)
+        {
+            $tries = $this->get('session')->get('tries');
+            $this->get('session')->set('tries', $tries+1);
+        }
+
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
+
+        $tries = $this->get('session')->get('tries');
+
+        $defaultData = [];
+        $form = $this->createFormBuilder($defaultData)
+            ->add('username', TextType::class)
+            ->add('password', PasswordType::class)
+            ->add('send', SubmitType::class)
+            ->add('captcha', CaptchaType::class)
+            ->getForm();
 
         return $this->render('security/login.html.twig', array(
             'last_username' => $lastUsername,
             'error'         => $error,
+            'form'          => $form->createView(),
+            'tries'         => $tries,
         ));
     }
 }
--- src/EventListener/LoginListener.php
+++ src/EventListener/LoginListener.php
@@ -4,15 +4,18 @@ namespace App\EventListener;
 
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use App\Entity\User;
 
 class LoginListener
 {
     private $entityManager;
+    private $session;
 
-    public function __construct(EntityManagerInterface $em)
+    public function __construct(EntityManagerInterface $em, SessionInterface $session)
     {
         $this->entityManager = $em;
+        $this->session = $session;
     }
 
     public function onSecurityInteractiveLogin(InteractiveLoginEvent $event)
@@ -26,6 +29,9 @@ class LoginListener
         # Update the DB
         $this->entityManager->persist($user);
         $this->entityManager->flush();
+
+        # Reset tries
+        $this->session->set('tries', 0);
     }
 }
 
